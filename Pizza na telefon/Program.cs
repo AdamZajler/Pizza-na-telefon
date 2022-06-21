44641bf128868f4a418c68acf56cbc9cc780f903
Adam Zajler <adamzajler01@gmail.com>
2022-06-21 18:22:17 +0200

    Dodaj pliki projektów.

---
+++ Pizza na telefon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pizza_na_telefon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
