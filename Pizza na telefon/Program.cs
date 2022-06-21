54bf2a038b7add283c558f5b453ec0ff01822233
Adam Zajler <adamzajler01@gmail.com>
2022-06-21 20:04:21 +0200

    Added basic functionality

--- Pizza na telefon/Program.cs
+++ Pizza na telefon/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Threading;
 
 namespace Pizza_na_telefon
 {
+    public static class GlobalData
+    {
+        public static int[] MenuPositions = new int[99];
+    };
     public class Funtions
     {
         public void CustomConsoleWriteLine(string text, string  text_color = "white", bool is_centered = false)
@@ -32,13 +37,73 @@ namespace Pizza_na_telefon
             Console.ResetColor();
         }
     }
+
+    public class Pizzeria
+    {
+        public Pizzeria()
+        {
+            var functions = new Funtions();
+
+            functions.CustomConsoleWriteLine("Witaj w naszej Pizzeri Italiano", "green", true);
+            functions.CustomConsoleWriteLine("Zapraszamy do złożenia zamówienia", "", true);
+            //Thread.Sleep(2500); //przerwa w wykonywaniu kodu na 2.5s
+
+            functions.CustomConsoleWriteLine("\nOto menu dla Ciebie", "", true);
+
+            this.Menu();
+
+            Console.WriteLine("\n\nAby zamknąć program w dowolnej chwili kliknij klaiwsz 'esc'");
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+            {
+                this.KoniecProgramu();
+            }
+        }
+        public void Menu()
+        {
+            var functions = new Funtions();
+            functions.CustomConsoleWriteLine("\n\nNapoje", "green", true); //środek
+            functions.CustomConsoleWriteLine("\nCola\t\t7zł");
+
+            for(int i=0; i<99; i++)
+            {
+                if(i == 0)
+                {
+                    functions.CustomConsoleWriteLine("\nCo zamawiasz (podaj numer pozycji)?");
+                }
+                else
+                {
+                    functions.CustomConsoleWriteLine("\nCzy chcesz zamówić coś jeszcze? ('0' => koniec składania zamówienia)");
+                }
+                var value = int.Parse(Console.ReadLine());
+                if(value == 0)
+                {
+                    Console.WriteLine("czyszczenie + przejscie do zamowienia");
+                    for (int j = 0; j <= GlobalData.MenuPositions.Length; j++)
+                    {
+                        if(GlobalData.MenuPositions[j] == 0)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine("no to: " + GlobalData.MenuPositions[j]);
+                    }
+                    return;
+                }
+                GlobalData.MenuPositions[i] = value;
+            }
+        }
+
+        public void KoniecProgramu()
+        {
+            Console.WriteLine("Koniec programu");
+            return;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             var functions = new Funtions();
-            functions.CustomConsoleWriteLine("hi", "red");
-            functions.CustomConsoleWriteLine("co tam", "", true);
+            var pizzeria = new Pizzeria();
         }
     }
 }
