08abe7627bfe87ab25a96e5fbfb35a16db276896
Adam Zajler <adamzajler01@gmail.com>
2022-06-21 19:15:16 +0200

    Added basic color function for displaying text

--- Pizza na telefon/Program.cs
+++ Pizza na telefon/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Pizza_na_telefon
 {
+    public class Funtions
+    {
+        public void CustomConsoleWriteLine(string text, string  text_color = "white", bool is_centered = false)
+        {
+            switch (text_color)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            
+            if (is_centered)
+            {
+                Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.CursorTop);
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+
+            Console.ResetColor();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var functions = new Funtions();
+            functions.CustomConsoleWriteLine("hi", "red");
+            functions.CustomConsoleWriteLine("co tam", "", true);
         }
     }
 }
