b7d914a1bc2f8ea2c1e7f4cfc8deed0b8596f5e7
Patrycja 131893 <chx94179@student.chorzow.wsb.pl>
2022-06-23 17:13:05 +0200

    UI & UX changes

--- Pizza na telefon/Program.cs
+++ Pizza na telefon/Program.cs
@@ -10,7 +10,7 @@ namespace Pizza_na_telefon
         public static string KlientNazwisko;
         public static string KlientAdres;
         public static string KlientUwagiDoZamowienia;
-        public static string KlientMetodaPlatnosci;
+        public static int KlientMetodaPlatnosci;
     };
     public class Funtions
     {
@@ -22,6 +22,9 @@ namespace Pizza_na_telefon
                     Console.ForegroundColor = ConsoleColor.Red;
                     break;
                 case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "Green":
                     Console.ForegroundColor = ConsoleColor.Green;
                     break;
                 default:
@@ -66,10 +69,26 @@ namespace Pizza_na_telefon
         public void Menu()
         {
             var functions = new Funtions();
-            functions.CustomConsoleWriteLine("\n\nNapoje", "green", true); //środek
-            functions.CustomConsoleWriteLine("\nCola\t\t7zł");
+            Console.WriteLine("\n\n");
+            functions.CustomConsoleWriteLine("Napoje", "green", true); //środek
+            Console.WriteLine("\n");
+            functions.CustomConsoleWriteLine("1.Cola           \t7zł","", true);
+            functions.CustomConsoleWriteLine("2.Fanta          \t7zł","",true);
+            functions.CustomConsoleWriteLine("3.Sprite          \t7zł","",true);
+            functions.CustomConsoleWriteLine("4.Woda gazowana  \t7zł","",true);
+            functions.CustomConsoleWriteLine("5.Woda niegazowana\t7zł","",true);
 
-            for(int i=0; i<GlobalData.MenuPositions.Length; i++)
+            Console.WriteLine("\n\n");
+            functions.CustomConsoleWriteLine("Pizza", "green", true); //środek
+            Console.WriteLine("\n");
+            functions.CustomConsoleWriteLine("6.Margherita \t\t18zł","", true);
+            functions.CustomConsoleWriteLine("7.Neapolitana\t\t22zł","", true);
+            functions.CustomConsoleWriteLine("8.Carbonara  \t\t25zł", "", true);
+            functions.CustomConsoleWriteLine("9.Mexico      \t30zł", "", true);
+            functions.CustomConsoleWriteLine("10.Rukola    \t\t35zł", "", true);
+            functions.CustomConsoleWriteLine("11.Hawaii    \t\t15zł", "", true);
+
+            for (int i=0; i<GlobalData.MenuPositions.Length; i++)
             {
                 if(i == 0)
                 {
@@ -91,11 +110,40 @@ namespace Pizza_na_telefon
 
         public void Szczegoly_Zamowienia()
         {
+
+
+            var functions = new Funtions();
             Console.WriteLine("czyszczenie + przejscie do zamowienia' czyszczenie 3s, może jakiś loader?");
             Thread.Sleep(3000);
             Console.Clear();
-            Console.WriteLine("Wprowadzanie danych osobowych, adresu, itp. bez walidacji");
+            functions.CustomConsoleWriteLine("Wprowadź swoje imie","Green", false);
+            GlobalData.KlientImie = Console.ReadLine();
+            functions.CustomConsoleWriteLine("Wprowadź swoje nazwisko","Green", false);
+            GlobalData.KlientNazwisko = Console.ReadLine();
+            functions.CustomConsoleWriteLine("Wprowadź miasto, ulice i numer domu/mieszkania","Green", false);
+            GlobalData.KlientAdres = Console.ReadLine();
+            functions.CustomConsoleWriteLine("Uwagi do zamówienia", "Green", false);
+            GlobalData.KlientUwagiDoZamowienia = Console.ReadLine();
+            functions.CustomConsoleWriteLine("Dostępne metody płatności:", "Green", false);
+            Console.WriteLine("\n1.Karta płatnicza\n2.Płatność gotówką przy odbiorze");
+            bool bledna_platnosc = false;
+            do
+            {
+                Console.WriteLine("\nWybierz metode płatności");
+                GlobalData.KlientMetodaPlatnosci = int.Parse(Console.ReadLine());
 
+                switch (GlobalData.KlientMetodaPlatnosci)
+                {
+                    case 1 :
+                        Console.WriteLine("wybrana metoda płatności to płatność kartą");
+                        bledna_platnosc = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wybrana metoda płatności nie istnieje");
+                        break;
+                }
+
+            } while (bledna_platnosc == false);
 
 
             Console.WriteLine("Naciśnij dowolny klawisz aby przejśc do podsumowania");
